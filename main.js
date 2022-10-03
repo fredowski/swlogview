61f83966eb109841b5b5c452a12547fb631017fa
Friedrich Beckmann <friedrich.beckmann@gmx.de>
2022-10-04 00:26:32 +0200

    plot BARO.Alt as default plot

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function readfiles(files) {
 	lf = new logfile(buffer);
 	document.logfile = lf;
 	build_navbar (lf.get_msgitem_list());
-	var ds = lf.get_data_series("RCIN", "C1");
+	var ds = lf.get_data_series("BARO", "Alt");
 	// Plot
 	ds.type = 'scatter';
 	ds.showlegend = true;
